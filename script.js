3b178504f5e928c51079041a5db43fe3f4a815b9
chat2-dev <srustipradhan385@gmail.com>
2024-12-28 16:35:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+
 function toggleMenu() {
-    const menu = document.getElementById("menu");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
+  const menuBar = document.querySelector(".menu-bar");
+  const menuIcon = document.querySelector(".menu-icon");
+
+  menuBar.classList.toggle("open");
+  menuIcon.classList.toggle("open");
 }
+
