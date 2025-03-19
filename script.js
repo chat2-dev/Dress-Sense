c3e17aa622678705e9a42f3df6f388d171fe3167
chat2-dev <srustipradhan385@gmail.com>
2025-03-19 10:16:58 +0000

    refactor: simplify menu structure and remove unused JavaScript functionality

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-fetch("../menu.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("menu-placeholder").innerHTML = data;
-    });
-
 // Load the menu bar for all pages
 fetch('../menu.html')
     .then(response => response.text())
